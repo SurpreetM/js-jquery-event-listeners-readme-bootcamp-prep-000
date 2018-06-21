ca65376a76e1ccff6b58f666676c8e5c3f6fbea4
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-06-21 01:39:06 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,26 @@
 //define functions here
+function getIt () {
+  $('p').on("click", function () {
+    alert("Hey!")
+  })
+}
+
+
+function frameIt () {
+  $('img').on("load", function () {
+    $('img').addClass('tasty')
+  })
+}
+
+
+function pressIt () {
+  $('#typing').on("keydown", function (key) {
+    if (key.which == 71) {
+      alert("You pressed 'g'!") }
+  })
+}
+
+
 
 $(document).ready(function(){
 
